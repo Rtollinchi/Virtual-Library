c5f62b54efc6e3cba82dde6be4f1c97fb13e009e
bjjones8512 <bjjones8512@yahoo.com>
2024-09-10 11:48:44 -0500

    book

--- classes/Book.js
+++ classes/Book.js
@@ -1,6 +1,10 @@
 // import the Media class:
-
+const Media = require('./Media');
 // create your Book class:
-
+class Book extends Media {
+    constructor(title, year, genre, author, numPages, rating) {
+        super(title, year, genre)
+    }
+}
 // don't change below
 module.exports = Book;
